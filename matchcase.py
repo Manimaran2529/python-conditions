1aaa226053a10dfdf754b00ca1b73e26ad9dbb57
manimaran <manimaranh9965@gmail.com>
2026-02-13 12:15:24 +0530

    matchcase

--- matchcase.py
+++ matchcase.py
@@ -1,11 +1,15 @@
-
-n=int(input("enter a number "))
-match n:
-    case 1:
-        print("monday")
-    case 2:
-        print("tuesday")
+a=input("enter a query to check ,checkbalance,withdraw,deposit:").lower()
+match a:
+    case "checkbalance":
+        print("80000")
+    case "withdraw":
+        b=int(input("enter a withdraw amount:"))
+        if b<=80000:
+            print("withdraw successfully")
+        else:
+            print("insuffient balance")
+    case "deposit":
+        deposit=int(input("enter a desposit amount"))
+        print(deposit,"successfully")
     case _:
-        print("invalid")
-
-    
+        print("enter a vaild query")
