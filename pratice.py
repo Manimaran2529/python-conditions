a15b2fa2f6df3c9d4c19fa6c3eceefd867aec8d8
manimaran <manimaranh9965@gmail.com>
2026-02-15 17:43:04 +0530

    ifelse

---
+++ pratice.py
@@ -0,0 +1,38 @@
+                  #if else"
+
+#Write a program to check whether a number is positive or negative.
+"""a=int(input("enter a number to check positive or negative:"))
+if a>=0:
+   print("positive numbers" )
+else:
+    print("neagtive number")"""
+
+#Check whether a number is even or odd.
+"""a=int(input("enter a number to check add or even:"))
+if a%2==0:
+    print("even number",a)
+else:
+    print("odd number",a)"""
+
+#Check whether a person is eligible to vote (age ≥ 18).
+"""age=int(input("enter your age:"))
+if(age>=18):
+    print("your are eligible for vote")
+else:
+    print("your are not eligible for vote")"""
+
+#check whether a number is divisible by 5
+"""number=int(input("Enter a number for checking:"))
+if (number%5==0):
+    print(number,"its divible by 5")
+else:
+    print(number,"its not divisble by 5 ")"""
+#Check whether a character is a vowel or consonant.
+"""vowels="aeiou"
+special="@#!$%^&*(){}[];"
+word=input("enter a word:")
+if any(letter in vowels or letter in special for letter in word):
+    print("not consonant")
+else:
+    print("consonants")"""
+
